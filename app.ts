2e8edb62a0b143fe637cd31bfa49018e299dc1df
Hung Luu <hungluu111103@gmail.com>
2023-01-30 16:48:10 +0700

    commit

--- app.ts
+++ app.ts
@@ -28,7 +28,7 @@ app.use(cookies());
 //   }
 // }
 
-app.use(cors({ origin: "*"}));
+app.use(cors({ origin: "https://job-board-client-zeta.vercel.app"}));
 
 app.use('/', defaultRouter)
 app.use('/jobs', jobRouter)
