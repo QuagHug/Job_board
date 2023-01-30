33782db2bad1ba7460b44c47a5747a9bf62d35e7
Hung Luu <hungluu111103@gmail.com>
2023-01-30 17:41:57 +0700

    commit

--- app.ts
+++ app.ts
@@ -27,12 +27,7 @@ app.use(cookies());
 //     }
 //   }
 // }
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type');
-    next(); 
-})
+app.use(cors());
 // app.options('*', cors())
 
 app.use('/', defaultRouter)
