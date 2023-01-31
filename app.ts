6fb77760e36ded08c705e3d8201cce3dbface8a4
Hung Luu <hungluu111103@gmail.com>
2023-01-31 13:42:20 +0700

    commit

--- app.ts
+++ app.ts
@@ -12,8 +12,8 @@ import cors from "cors"
 
 const app = express()
 
+app.use(cors());
 app.options('/', cors());
-// app.use(cors());
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());   
