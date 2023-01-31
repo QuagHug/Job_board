450773329bed577c036aa3e0b9caa8b6dc916ab2
Hung Luu <hungluu111103@gmail.com>
2023-01-31 11:57:28 +0700

    commit

--- app.ts
+++ app.ts
@@ -12,7 +12,15 @@ import cors from "cors"
 
 const app = express()
 
-app.use(cors());
+// app.use(cors());
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());   
