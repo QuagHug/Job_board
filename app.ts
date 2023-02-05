d45a11c561db868b3f7aa8050fa35a55945a3dec
Hung Luu <hungluu111103@gmail.com>
2023-02-05 17:10:04 +0700

    commit

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ dotenv.config();
 
 
 const app = express()
-app.use(cors({origin: ["https://job-board-client-zeta.vercel.app"] , credentials: true, allowedHeaders: ["Origin","X-Requested-With","content-type","set-cookie"] }));
+app.use(cors({origin: ["https://job-board-client-zeta.vercel.app"] , credentials: true, allowedHeaders: ["Origin","X-Requested-With","content-type","set-cookie", "jwt"] }));
 
 
 app.use(express.urlencoded({extended: true})); 
